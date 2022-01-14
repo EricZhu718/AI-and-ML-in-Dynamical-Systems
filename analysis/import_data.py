93643c598e2f901a0404b48edaa1ab05d4bbf9a2
Eric Zhu <ezhu2009@gmail.com>
2022-01-14 03:03:02 -0500

    Bug fixes

--- analysis/import_data.py
+++ analysis/import_data.py
@@ -4,14 +4,12 @@ import pandas as pd
 import yfinance as yf
 
 
-def makeCSV(start_date, end_date):
-
-    ticker = '^GSPC'
+def getCSV(start_date, end_date, ticker = '^GSPC'):
     data = yf.download(ticker, start_date, end_date)
     print(data)
-    return data
+    return data.to_csv(index = False)
 
 
 if __name__ == '__main__':
-    makeCSV('1990-01-01', '2021-07-12')
+    print(getCSV('1990-01-01', '2021-07-12'))
 
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(express.static("express"));
 // default URL for website
 app.use('/', function(req,res){
-    res.sendFile(path.join(__dirname+'/express/index.html'));
+    res.sendFile(path.join(__dirname+'/index.html'));
     //__dirname : It will resolve to your project folder.
   });
 const server = http.createServer(app);
@@ -14,7 +14,3 @@ const port = 3000;
 server.listen(port);
 console.debug('Server listening on port ' + port);
 console.log('Began Running at ' + (new Date()).toString())
-
-
-
-
